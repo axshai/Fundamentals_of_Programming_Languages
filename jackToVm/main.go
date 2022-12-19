1f1ce986118ea3e21dfd503b141021ae5fe4609b
USER <ashmenzer@gmail.com>
2022-12-19 10:12:06 +0200

    add regex for multiComment and recursive call for all the directory

--- jackToVm/main.go
+++ jackToVm/main.go
@@ -1,16 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	t := newToknizer("shai", "Main.jack")
+	pathFinder(os.Args[1])
+}
+
+// The function recives a path to the project root folder (e.g. pro 10)
+// and for each folder that contains jack files it triggers the compilation process to create token xml file
+// in the same folder.
+func pathFinder(rootPath string) {
+	jackF, _ := regexp.Compile(".*[.]jack")
+	files, err := os.ReadDir(rootPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// loop over the items in the dir
+	for _, item := range files {
+		// if it is a dir: make recursive call
+		if item.IsDir() {
+			pathFinder(filepath.Join(rootPath, item.Name()))
+			// else - it is a jack files directory
+		} else {
+			// if it is a jack file - translate it to tokens xml
+			if jackF.MatchString(item.Name()) {
+				currentFile = item.Name()
+				outPutFile := strings.Split(currentFile, ".")[0] + "Tk.xml"
+				outPutFile = filepath.Join(rootPath, outPutFile)
+				currentFile = filepath.Join(rootPath, currentFile)
+				fmt.Println(outPutFile)
+				jackToTokensTraslator(currentFile, outPutFile)
+			}
+		}
+	}
+}
+
+// The function recives a path a to jack file, and path to the output file
+// and translate the jack file into tokens inside xml file in the output file
+func jackToTokensTraslator(inputFile string, outputFile string) {
+	// Clean from files from previous runs
+	if _, err := os.Stat(outputFile); err == nil {
+		os.Remove(outputFile)
+	}
+	// create the tokenizer struct to translate the file
+	t := newToknizer(outputFile, inputFile)
 	for t.isThereMoreTokens() {
 		typ, token := t.nextToken()
-		fmt.Println(typ, token)
-		if typ != comment && typ != b {
+		if typ != comment && typ != multiComment && typ != b {
 			t.writeToken(tokenTypeMap[typ], translateToken(token))
 		}
 	}
 	t.closeToknizer()
-
 }
--- jackToVm/syntax.go
+++ jackToVm/syntax.go
@@ -7,33 +7,48 @@ import (
 	"strings"
 )
 
+var currentFile string
+
+// the
 type Toknizer struct {
 	file         *os.File
 	tokensString string
 }
 
+// constructor to Toknizer get the jack file to tokinze and the
+// file name to where to write the tokens xml file to
 func newToknizer(fileName string, jackFile string) Toknizer {
-	f, _ := os.OpenFile(fileName+".xml", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, _ := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	tokens, _ := os.ReadFile(jackFile)
+	// delete the spacess
 	tokensString := string(strings.TrimSpace(string(tokens)))
+	//the root of the xml file
 	f.WriteString("<tokens>\n")
 	return Toknizer{file: f, tokensString: string(tokensString)}
 }
 
+// function to write token that get the token type and the token value
+// and create token like this <type> value </type>
 func (t Toknizer) writeToken(tokentType string, token string) {
 	strToken := fmt.Sprintf("<%s> %s </%s>", tokentType, token, tokentType)
 	t.file.WriteString(strToken + "\n")
 }
 
+// function to finish the toknizer
 func (t Toknizer) closeToknizer() {
+	//the close for the root of the xml
 	t.file.WriteString("</tokens>\n")
+	//close the file
 	t.file.Close()
 }
 
+// function that return the type and value of the next token in the jack file
 func (t *Toknizer) nextToken() (token, string) {
-	for _, key := range []token{comment, stringConstant, keyword, symbol, identifier, integerConstant} {
+	// go over all the tokens type and search for match
+	for _, key := range []token{multiComment, comment, stringConstant, keyword, symbol, identifier, integerConstant} {
 		r, _ := regexp.Compile(tokenRegexMap[key])
 		if r.MatchString(t.tokensString) {
+			//if you found token delete it from the file and delete the spaces
 			token := r.FindString(t.tokensString)
 			t.tokensString = t.tokensString[strings.Index(t.tokensString, token)+len(token):]
 			t.tokensString = strings.TrimSpace(t.tokensString)
@@ -43,10 +58,12 @@ func (t *Toknizer) nextToken() (token, string) {
 	return err, "error"
 }
 
+// function to check if we finish to toknize the file
 func (t Toknizer) isThereMoreTokens() bool {
 	return t.tokensString != ""
 }
 
+// function to translate the token to xml version
 func translateToken(token string) string {
 	if token == "<" {
 		token = "&lt;"
@@ -65,6 +82,7 @@ func translateToken(token string) string {
 
 type token int
 
+// all the tokens type
 const (
 	keyword token = iota
 	symbol
@@ -72,6 +90,7 @@ const (
 	identifier
 	stringConstant
 	comment
+	multiComment
 	b
 	err
 )
@@ -79,13 +98,15 @@ const (
 // map to check the token type type by regex
 var tokenRegexMap = map[token]string{
 	keyword:         `^(\b(?:class|method|function|constructor|int|boolean|char|void|var|static|field|let|do|if|else|while|return|true|false|null|this)\b)`,
-	symbol:          `^({|}|\[|\]|\(|\)|\.|,|;|\+|-|\*|\/|&|\||<|>|=|~)`,
+	symbol:          "^({|}|\\[|\\]|\\(|\\)|\\.|,|;|\\+|-|\\*|\\/|&|\\||<|>|=|~)",
 	integerConstant: "^([0-9])+",
 	identifier:      "^([a-zA-Z_][a-zA-Z_0-9]*)",
 	stringConstant:  "^(\"[^\n^\"]*\")",
-	comment:         `^((//.*\n)|(/\*.*\*/))`,
+	comment:         `^(//.*\n)`,
+	multiComment:    `(?s)^/\*.*?\*/`,
 }
 
+// map to get the string of the token for the xml file
 var tokenTypeMap = map[token]string{
 	keyword:         "keyword",
 	symbol:          "symbol",
