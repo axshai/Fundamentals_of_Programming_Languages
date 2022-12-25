011c6cb1533b1bb840a8ec80e1142d58b0083264
Shai Axelrod <Saxelrod@g.jct.ac.il>
2022-12-25 19:20:51 +0200

    add lookahaed func to replace backToPrevToken func
    in addition, add some doc

--- jackToVm/Parser.go
+++ jackToVm/Parser.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+// syntaxParser - Responsible for creating syntax tree (As xml file)
+// from tokens file
 type syntaxParser struct {
 	outFile      *os.File
 	indentations string
@@ -15,6 +17,7 @@ type syntaxParser struct {
 	lineNumber   int
 }
 
+// syntaxParser "constructor"
 func newParser(fileName string, tokensFileName string) syntaxParser {
 	f, _ := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	newP := syntaxParser{outFile: f, indentations: "", lineNumber: 0, tokensBuffer: [][]string{}}
@@ -22,13 +25,16 @@ func newParser(fileName string, tokensFileName string) syntaxParser {
 	return newP
 }
 
+// The function initializes the buffer of syntaxParser with all the tokens and their type
 func (p *syntaxParser) initTokensBuffer(tokensFileName string) {
 	tokensFile, _ := os.Open(tokensFileName)
 	fileScanner := bufio.NewScanner(tokensFile)
 	fileScanner.Split(bufio.ScanLines)
 	typeR, _ := regexp.Compile("</.*>")
 	tokenR, _ := regexp.Compile(">.*<")
+	// for each line in the tokens xml
 	for fileScanner.Scan() {
+		// if kine contains token - extract the token and its type
 		if tokenR.MatchString(fileScanner.Text()) {
 			tokenStr := tokenR.FindString(fileScanner.Text())
 			typeRStr := typeR.FindString(fileScanner.Text())
@@ -38,7 +44,9 @@ func (p *syntaxParser) initTokensBuffer(tokensFileName string) {
 	tokensFile.Close()
 }
 
+// The function return the next token and its type
 func (p *syntaxParser) getNextToken() (string, string) {
+	// if ther are not more tokens - return empty strings
 	if p.lineNumber >= len(p.tokensBuffer) {
 		return "", ""
 	}
@@ -47,26 +55,37 @@ func (p *syntaxParser) getNextToken() (string, string) {
 	return tType, strings.TrimSpace(token)
 }
 
-func (p *syntaxParser) backToPrevToken() {
-	p.lineNumber--
+func (p *syntaxParser) lookahead(steps int) (string, string) {
+	// if ther are not more tokens - return empty strings
+	if (p.lineNumber + steps - 1) >= len(p.tokensBuffer) {
+		return "", ""
+	}
+	tType, token := p.tokensBuffer[p.lineNumber+steps-1][0], p.tokensBuffer[p.lineNumber+steps-1][1]
+	return tType, strings.TrimSpace(token)
 }
 
+// function to manage indentations in the syntax xml file - increase the indentations
 func (p *syntaxParser) increasIndentation() {
 	p.indentations += "  "
 }
 
+// function to manage indentations in the syntax xml file - decrease the indentations
 func (p *syntaxParser) decreasIndentation() {
 	p.indentations = p.indentations[:len(p.indentations)-2]
 }
 
+// the function gets token type and the token value
+// and write the token in the following format: <type> value </type>
 func (p syntaxParser) writeToken(tokentType string, token string) {
 	strToken := fmt.Sprintf("%s<%s> %s </%s>", p.indentations, tokentType, token, tokentType)
 	p.outFile.WriteString(strToken + "\n")
 }
 
-func (p *syntaxParser) writeBlockTag(blockName string, closeTag bool) {
+// the function writes start/end (according to isCloseTag) block tag
+// and changes the indents accordingly
+func (p *syntaxParser) writeBlockTag(blockName string, isCloseTag bool) {
 	tag := ""
-	if !closeTag {
+	if !isCloseTag {
 		tag = fmt.Sprintf("%s<%s>", p.indentations, blockName)
 		p.increasIndentation()
 	} else {
@@ -76,6 +95,7 @@ func (p *syntaxParser) writeBlockTag(blockName string, closeTag bool) {
 	p.outFile.WriteString(tag + "\n")
 }
 
+// function to finish the toknizer
 func (p syntaxParser) closeToknizer() {
 	p.outFile.Close()
 }
@@ -92,31 +112,30 @@ func parseClass(p syntaxParser) {
 }
 
 func ParseClassVarDec(p *syntaxParser) {
-	tType, token := p.getNextToken()
+	_, token := p.lookahead(1)
 	for token == "static" || token == "field" {
 		p.writeBlockTag("classVarDec", false)
-		p.writeToken(tType, token)     // field || static
+		p.writeToken(p.getNextToken()) // field || static
 		p.writeToken(p.getNextToken()) //<keyword> int </keyword> || <identifier> className </identifier>
 		p.writeToken(p.getNextToken()) //<identifier> x </identifier>
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 		for token == "," {
-			p.writeToken(tType, token)     // <symbol> , </symbol>
+			p.writeToken(p.getNextToken()) // <symbol> , </symbol>
 			p.writeToken(p.getNextToken()) //<identifier> y </identifier>
-			tType, token = p.getNextToken()
+			_, token = p.lookahead(1)
 		}
-		p.backToPrevToken()
 		p.writeToken(p.getNextToken()) //<identifier> ; </identifier>
 		p.writeBlockTag("classVarDec", true)
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.backToPrevToken()
+
 }
 
 func ParseSubRoutineDec(p *syntaxParser) {
-	tType, token := p.getNextToken()
+	_, token := p.lookahead(1)
 	for token == "constructor" || token == "method" || token == "function" {
 		p.writeBlockTag("subroutineDec", false)
-		p.writeToken(tType, token)     // method || constructor || function
+		p.writeToken(p.getNextToken()) // method || constructor || function
 		p.writeToken(p.getNextToken()) //<keyword> type </keyword>
 		p.writeToken(p.getNextToken()) //<identifier> funcNmae </identifier>
 		p.writeToken(p.getNextToken()) //<symbol> ( </symbol>
@@ -124,28 +143,25 @@ func ParseSubRoutineDec(p *syntaxParser) {
 		p.writeToken(p.getNextToken()) //<symbol> ) </symbol>
 		ParseSubRoutineBody(p)
 		p.writeBlockTag("subroutineDec", true)
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.backToPrevToken()
 }
 
 func ParseParameterList(p *syntaxParser) {
-	tType, token := p.getNextToken()
+	_, token := p.lookahead(1)
 	p.writeBlockTag("parameterList", false)
 	for token == "int" || token == "void" || token == "char" || token == "boolean" {
-		p.writeToken(tType, token)     //<keyword> type </keyword>
+		p.writeToken(p.getNextToken()) //<keyword> type </keyword>
 		p.writeToken(p.getNextToken()) //<identifier> varName </identifier>
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 		for token == "," {
-			p.writeToken(tType, token)     // <symbol> , </symbol>
+			p.writeToken(p.getNextToken()) // <symbol> , </symbol>
 			p.writeToken(p.getNextToken()) //<keyword> type </keyword>
 			p.writeToken(p.getNextToken()) //<identifier> varName </identifier>
-			tType, token = p.getNextToken()
+			_, token = p.lookahead(1)
 		}
-		p.backToPrevToken()
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.backToPrevToken()
 	p.writeBlockTag("parameterList", true)
 
 }
@@ -159,81 +175,77 @@ func ParseSubRoutineBody(p *syntaxParser) {
 }
 
 func ParsevarDec(p *syntaxParser) {
-	tType, token := p.getNextToken()
+	_, token := p.lookahead(1)
 	for token == "var" {
 		p.writeBlockTag("varDec", false)
-		p.writeToken(tType, token)     //<keyword> var </keyword>
+		p.writeToken(p.getNextToken()) //<keyword> var </keyword>
 		p.writeToken(p.getNextToken()) //<keyword> type </keyword>
 		p.writeToken(p.getNextToken()) //<identifier> varName </identifier>
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 		for token == "," {
-			p.writeToken(tType, token)     // <symbol> , </symbol>
+			p.writeToken(p.getNextToken()) // <symbol> , </symbol>
 			p.writeToken(p.getNextToken()) //<identifier> varName </identifier>
-			tType, token = p.getNextToken()
+			_, token = p.lookahead(1)
 		}
-		p.backToPrevToken()
 		p.writeToken(p.getNextToken()) //<keyword> ; </keyword>
 		p.writeBlockTag("varDec", true)
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.backToPrevToken()
 }
 
 func ParseStatments(p *syntaxParser) {
-	var handler func(*syntaxParser, string, string)
-	tType, token := p.getNextToken()
+	var handler func(*syntaxParser)
+	_, token := p.lookahead(1)
 	exists := false
 	p.writeBlockTag("statements", false)
 	handler, exists = statmentHandlersMap[token]
 	for exists {
-		handler(p, tType, token)
-		tType, token = p.getNextToken()
+		handler(p)
+		_, token = p.lookahead(1)
 		handler, exists = statmentHandlersMap[token]
 	}
-	p.backToPrevToken()
 	p.writeBlockTag("statements", true)
 }
 
-func letStatment(p *syntaxParser, tType string, token string) {
+func letStatment(p *syntaxParser) {
 	p.writeBlockTag("letStatement", false)
-	p.writeToken(tType, token)     //let
+	p.writeToken(p.getNextToken()) //let
 	p.writeToken(p.getNextToken()) //<identifier> varName </identifier>
-	tType, token = p.getNextToken()
+	_, token := p.lookahead(1)
 	if token == "[" {
-		p.writeToken(tType, token)
+		p.writeToken(p.getNextToken())
 		ParseExpression(p)
 		p.writeToken(p.getNextToken())
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.writeToken(tType, token) // =
+	p.writeToken(p.getNextToken()) // =
 	ParseExpression(p)
 	p.writeToken(p.getNextToken()) //;
 	p.writeBlockTag("letStatement", true)
 }
 
-func ifStatment(p *syntaxParser, tType string, token string) {
+func ifStatment(p *syntaxParser) {
 	p.writeBlockTag("ifStatement", false)
-	p.writeToken(tType, token)     // if
+	p.writeToken(p.getNextToken()) // if
 	p.writeToken(p.getNextToken()) //(
 	ParseExpression(p)
 	p.writeToken(p.getNextToken()) //)
 	p.writeToken(p.getNextToken()) //{
 	ParseStatments(p)
 	p.writeToken(p.getNextToken()) //}
-	tType, token = p.getNextToken()
+	_, token := p.lookahead(1)
 	if token == "else" {
-		p.writeToken(tType, token)     //else
+		p.writeToken(p.getNextToken()) //else
 		p.writeToken(p.getNextToken()) //{
 		ParseStatments(p)
 		p.writeToken(p.getNextToken()) //}
-	} else {
-		p.backToPrevToken()
 	}
 	p.writeBlockTag("ifStatement", true)
 }
-func whileStatment(p *syntaxParser, tType string, token string) {
+
+func whileStatment(p *syntaxParser) {
 	p.writeBlockTag("whileStatement", false)
-	p.writeToken(tType, token)     // while
+	p.writeToken(p.getNextToken()) // while
 	p.writeToken(p.getNextToken()) //(
 	ParseExpression(p)
 	p.writeToken(p.getNextToken()) //)
@@ -243,24 +255,23 @@ func whileStatment(p *syntaxParser, tType string, token string) {
 	p.writeBlockTag("whileStatement", true)
 
 }
-func doStatment(p *syntaxParser, tType string, token string) {
+func doStatment(p *syntaxParser) {
 	p.writeBlockTag("doStatement", false)
-	p.writeToken(tType, token) // do
+	p.writeToken(p.getNextToken()) // do
 	ParseSubRoutineCall(p)
 	p.writeToken(p.getNextToken()) // ;
 	p.writeBlockTag("doStatement", true)
 
 }
-func returnStatment(p *syntaxParser, tType string, token string) {
+func returnStatment(p *syntaxParser) {
 	p.writeBlockTag("returnStatement", false)
-	p.writeToken(tType, token) // return
-	tType, token = p.getNextToken()
+	p.writeToken(p.getNextToken()) // return
+	_, token := p.lookahead(1)
 	if token != ";" {
-		p.backToPrevToken()
 		ParseExpression(p)
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.writeToken(tType, token) // ;
+	p.writeToken(p.getNextToken()) // ;
 	p.writeBlockTag("returnStatement", true)
 
 }
@@ -268,45 +279,41 @@ func returnStatment(p *syntaxParser, tType string, token string) {
 func ParseExpression(p *syntaxParser) {
 	p.writeBlockTag("expression", false)
 	ParseTerm(p)
-	tType, token := p.getNextToken()
+	_, token := p.lookahead(1)
 	for strings.Contains("+-*/|=", token) || token == "&amp;" || token == "&gt;" || token == "&lt;" {
-		p.writeToken(tType, token)
+		p.writeToken(p.getNextToken())
 		ParseTerm(p)
-		tType, token = p.getNextToken()
+		_, token = p.lookahead(1)
 	}
-	p.backToPrevToken()
 	p.writeBlockTag("expression", true)
 }
 
 func ParseTerm(p *syntaxParser) {
 	p.writeBlockTag("term", false)
-	tType, token := p.getNextToken()
+	tType, token := p.lookahead(1)
 	switch tType {
 	case tokenTypeMap[integerConstant], tokenTypeMap[stringConstant], tokenTypeMap[keyword]:
-		p.writeToken(tType, token)
+		p.writeToken(p.getNextToken())
 	case tokenTypeMap[symbol]:
 		if token == "-" || token == "~" {
-			p.writeToken(tType, token)
+			p.writeToken(p.getNextToken())
 			ParseTerm(p)
 		} else { // (
-			p.writeToken(tType, token)
+			p.writeToken(p.getNextToken())
 			ParseExpression(p)
 			p.writeToken(p.getNextToken()) // )
 		}
 	case tokenTypeMap[identifier]:
-		tType1, token1 := p.getNextToken()
+		_, token1 := p.lookahead(2)
 		if token1 == "(" || token1 == "." {
-			p.backToPrevToken()
-			p.backToPrevToken()
 			ParseSubRoutineCall(p)
 		} else if token1 == "[" {
-			p.writeToken(tType, token)
-			p.writeToken(tType1, token1)
+			p.writeToken(p.getNextToken())
+			p.writeToken(p.getNextToken())
 			ParseExpression(p)
 			p.writeToken(p.getNextToken())
 		} else {
-			p.writeToken(tType, token)
-			p.backToPrevToken()
+			p.writeToken(p.getNextToken())
 		}
 	}
 	p.writeBlockTag("term", true)
@@ -315,8 +322,8 @@ func ParseTerm(p *syntaxParser) {
 func ParseSubRoutineCall(p *syntaxParser) {
 	//p.writeBlockTag("subRoutineCall", false)
 	p.writeToken(p.getNextToken())
-	tType, token := p.getNextToken()
-	p.writeToken(tType, token)
+	_, token := p.lookahead(1)
+	p.writeToken(p.getNextToken())
 	if token == "(" {
 		ParseExpressionList(p)
 		p.writeToken(p.getNextToken()) // )
@@ -330,19 +337,17 @@ func ParseSubRoutineCall(p *syntaxParser) {
 }
 func ParseExpressionList(p *syntaxParser) {
 	p.writeBlockTag("expressionList", false)
-	_, token := p.getNextToken()
-	p.backToPrevToken()
+	_, token := p.lookahead(1)
 	if token != ")" {
 		ParseExpression(p)
-		tType, token := p.getNextToken()
+		_, token := p.lookahead(1)
 		for token == "," {
-			p.writeToken(tType, token) // <symbol> , </symbol>
+			p.writeToken(p.getNextToken()) // <symbol> , </symbol>
 			ParseExpression(p)
-			tType, token = p.getNextToken()
+			_, token = p.lookahead(1)
 		}
-		p.backToPrevToken()
 	}
 	p.writeBlockTag("expressionList", true)
 }
 
-var statmentHandlersMap = map[string]func(*syntaxParser, string, string){}
+var statmentHandlersMap = map[string]func(*syntaxParser){}
